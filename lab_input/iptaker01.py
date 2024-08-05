4aa9e9632313366b44301807d58af778ed7fcc6c
katieraerae <catherinem@tlgcohort.com>
2024-08-05 21:46:33 +0000

    list objects and methods

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,11 @@
+"""Atlas Research
+    print() display data to stand out"""
+
+def main():
+    user_input = input("Please enter an IPv4 IP address:")
+    print("You told me the IPv4 address is:" + user_input)
+
+main()
+
+
+
---
+++ strings/stringmethod01.py
@@ -0,0 +1,5 @@
+lilstring = "Yeaaaaah. Oooooook. You can do it all by yourself"
+newlist = lilstring.split(" ")
+print(newlist)
+
+
