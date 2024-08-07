2f548a380a74fcda0c06c59d1f1c50585e35663c
katieraerae <catherinem@tlgcohort.com>
2024-08-07 19:13:55 +0000

    unfinished rename/move files

---
+++ ceph_storage/moveplease01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import shutil
+import os
+os.chdir('/home/student/mycode/')
+shutil.move('raynor.obj', 'ceph_storage/')
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
